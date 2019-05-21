9a505ae3f13f6a4e10661543dd8ea08d95e9c77c
Anton Zubochenko <zubochenko@gmail.com>
2019-05-21 11:50:17 +0300

    Quest 2.4.1 Text compressor

---
+++ 2.4.1.py
@@ -0,0 +1,15 @@
+word = input()
+x = 1
+word_2 = word[x:x+1]
+b = 1
+
+for i in word:
+    if i in word_2:
+        b +=1
+    else:
+        print (i, end = '')
+        print (b, end = '')
+        b = 1
+    x += 1
+    word_2 = word[x:x + 1]
+
